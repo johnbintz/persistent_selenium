24d7eccfe1ef8db2f20cdf6b31d6dcc4567172fa
John Bintz <john@coswellproductions.com>
2012-11-26 16:35:28 -0500

    add cool splash screen

--- lib/persistent_selenium/browser.rb
+++ lib/persistent_selenium/browser.rb
@@ -1,5 +1,6 @@
 require 'capybara'
 require 'capybara/selenium/driver'
+require 'base64'
 
 module PersistentSelenium
   class Browser < Capybara::Selenium::Driver
@@ -26,7 +27,7 @@ module PersistentSelenium
     def set_app_host(host)
       @app_host = host
 
-      browser.navigate.to('about:blank')
+      browser.navigate.to("data:text/html;base64,#{Base64.encode64(starting_page)}")
     end
 
     def reset!
@@ -39,6 +40,45 @@ module PersistentSelenium
           # instead.
         end
       end
+    end
+
+    def starting_page
+      <<-HTML
+<html>
+  <head>
+    <title>Persistent Selenium -- Starting up...</title>
+    <style>
+      body {
+        background: #444;
+      }
+
+      * {
+        font-family: Verdana, Helvetica, sans-serif;
+      }
+
+      td {
+        background: #ccc;
+        width: 50%;
+        text-align: center;
+
+        border: solid #333 1px;
+        border-radius: 5px;
+        padding: 10px;
+      }
+    </style>
+  </head>
+  <body>
+    <table width="100%" height="100%" cellpadding="0" cellspacing="0">
+      <tr>
+        <td>
+          <h1>Persistent Selenium</h1>
+          <h2>Starting up...</h2>
+        </td>
+      </tr>
+    </table>
+  </body>
+</html>
+      HTML
     end
   end
 end
